364a566365d381cc67e0962ceab735aeded599e6
dntsxh <miih.santana013@gmail.com>
2024-11-27 11:12:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'ONE PIECE',
+    'O que torna a Akuma no Mi (fruta do diabo) tão especial em "One Piece"?',
+    'Essas frutas místicas conferem habilidades sobrenaturais a quem os consome, mas também impõem uma condição única: a perda da habilidade de nadar'
+)
+criaCartao(
+    'GEOGRAFIA',
+    'Qual a capital da Espanha?',
+    'A capital da Espanha é Madri'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual foi o primeiro longa-metragem de animação da DisneyS?',
+     ' O primeiro longa-metragem de animação de Walt Disney foi Branca de Neve e os Sete Anões, deS 1937.'
+)
+
+criaCartao(
+    'JUJUTSU KAISEN',
+    'O que são maldições em "jujutsu kaisen"?',
+    ' São criadas quando energia amaldiçoada sai de (não-feiticeiros) humanos como resultado de suas emoções negativas'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
